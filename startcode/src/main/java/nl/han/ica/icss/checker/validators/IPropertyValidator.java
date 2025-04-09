9725933f104296c545c94e836f8cc59d05400f2e
Cedric <cm.vanlenthe@student.han.nl>
2025-04-09 21:47:01 +0200

    Removed unused validators.

--- startcode/src/main/java/nl/han/ica/icss/checker/validators/IPropertyValidator.java
+++
@@ -1,9 +0,0 @@
-package nl.han.ica.icss.checker.validators;
-
-import nl.han.ica.icss.ast.Declaration;
-import nl.han.ica.icss.ast.types.ExpressionType;
-import nl.han.ica.icss.checker.Checker;
-
-public interface IPropertyValidator {
-    void validate(Declaration declaration, ExpressionType type, Checker checker);
-}
